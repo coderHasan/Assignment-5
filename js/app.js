ec95b7e4ad12486883183708c717e0e30125131d
coderHasan <skmdmehedihasan420@gmail.com>
2024-09-24 00:29:23 +0600

    Feni Donate Section Done

--- js/app.js
+++ js/app.js
@@ -5,18 +5,19 @@ const inputCount = document.querySelector(".input-amount")
 const donatBtn = document.querySelector(".donateBtn")
 const modal = document.querySelector(".modal")
 const closeBtn = document.querySelector(".closeBtn")
-
+// Feni Donate
+const feniDonatBtn = document.querySelector(".feni-donate-btn")
 
 donatBtn.addEventListener("click", function() {
     const inputNumber = parseFloat(inputCount.value)
     const navCountNumber = parseFloat(navCount.innerText)
     const floodCountNumber = parseFloat(floodCount.innerText) 
    
-    if(!inputNumber <=0){
+    if(inputNumber > 0){
         modal.classList.add("modal-open")          
-        const newBlance =  floodCountNumber + inputNumber 
+        const newBlance =  floodCountNumber + inputNumber.toFixed(2) 
         floodCount.innerText = newBlance
-        const myNewBlance = navCountNumber - inputNumber
+        const myNewBlance = navCountNumber - inputNumber.toFixed(2)
         
         navCount.innerText = myNewBlance
         inputCount.focus()
@@ -24,13 +25,17 @@ donatBtn.addEventListener("click", function() {
         
     }else{
         
-        alert("Donate faild")
+        alert("Donate failed")
     }
 })
 closeBtn.addEventListener("click", function() {
     modal.classList.remove("modal-open")
 })
-
+feniDonatBtn.addEventListener("click", function() {
+    feniDonate(
+        navCount
+    )
+})
 
 
 
--- js/util.js
+++ js/util.js
@@ -1,1 +1,26 @@
 
+
+const feniFloodCount = document.querySelector("#feni-flood-amount")
+const feniCount = document.querySelector(".feni-amount")
+
+
+function feniDonate(navCount) {
+    const feniInputNumber = parseFloat(feniCount.value)
+    const feniNavCountNumber = parseFloat(navCount.innerText)
+    const floodCountNumber = parseFloat(feniFloodCount.innerText) 
+   
+    if(feniInputNumber > 0){
+        modal.classList.add("modal-open")          
+        const feniNewBlance =  floodCountNumber + feniInputNumber 
+        feniFloodCount.innerText = feniNewBlance
+        const myFeniNewBlance = feniNavCountNumber - feniInputNumber
+        
+        navCount.innerText = myFeniNewBlance
+        feniCount.focus()
+        feniCount.value = ''
+        
+    }else{
+        alert("Donate failed")
+    }
+
+}
