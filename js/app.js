f85afc08dc35e451a2e0b4645a87a09495ef8f02
coderHasan <skmdmehedihasan420@gmail.com>
2024-09-24 12:28:06 +0600

    History + quota done

--- js/app.js
+++ js/app.js
@@ -5,9 +5,26 @@ const inputCount = document.querySelector(".input-amount")
 const donatBtn = document.querySelector(".donateBtn")
 const modal = document.querySelector(".modal")
 const closeBtn = document.querySelector(".closeBtn")
+const cardTitle1 = document.querySelector(".title1")
 // Feni Donate
 const feniDonatBtn = document.querySelector(".feni-donate-btn")
+const historyContainer1 = document.getElementById("noakhali")
+const cardTitle2 = document.querySelector(".title2")
+// Quata Donate
+const quataDonatBtn = document.querySelector(".quata-donate-btn")
+const cardTitle3 = document.querySelector(".title3")
 
+
+//Donate & History Button
+const donateBtn = document.querySelector(".donate-btn")
+const historyBtn = document.querySelector(".history-btn")
+const main = document.querySelectorAll(".main")
+
+
+
+
+
+// Noakhali donate card
 donatBtn.addEventListener("click", function() {
     const inputNumber = parseFloat(inputCount.value)
     const navCountNumber = parseFloat(navCount.innerText)
@@ -15,27 +32,62 @@ donatBtn.addEventListener("click", function() {
    
     if(inputNumber > 0){
         modal.classList.add("modal-open")          
-        const newBlance =  floodCountNumber + inputNumber.toFixed(2) 
+        const newBlance =  floodCountNumber + inputNumber 
         floodCount.innerText = newBlance
-        const myNewBlance = navCountNumber - inputNumber.toFixed(2)
-        
+        const myNewBlance = navCountNumber - inputNumber
         navCount.innerText = myNewBlance
         inputCount.focus()
         inputCount.value = ''
+
+
+        const historyItems = document.createElement("div")
+        historyItems.innerHTML=`
+            
+
+          <div class="p-5 border border-gray-300 rounded-md mb-7">
+                <p class="text-md font-bold mb-5"> ${floodCount.innerText = newBlance} <span class="text-3xl font-extrabold">৳</span> ${cardTitle1.innerText}</p>
+
+                <p>${new Date()}</p>
+
+          </div>
+
+        `
+        historyContainer1.insertBefore(historyItems, historyContainer1.firstChild)
+    
         
     }else{
         
         alert("Donate failed")
     }
+
+   
 })
+
 closeBtn.addEventListener("click", function() {
     modal.classList.remove("modal-open")
 })
+
+// feni donate card
 feniDonatBtn.addEventListener("click", function() {
     feniDonate(
-        navCount
+        navCount, cardTitle2
     )
 })
+
+// Quata
+quataDonatBtn.addEventListener("click", function() {
+    quataDonate(navCount, cardTitle3)
+})
+
+//Style history  button
+historyBtn.addEventListener("click", function() {
+    historyButton(historyBtn, donateBtn, historyContainer1)
+    for(const section of main){
+            section.classList.add("hidden")
+    }
+
+})
+
 
 
 
--- js/util.js
+++ js/util.js
@@ -3,8 +3,13 @@
 const feniFloodCount = document.querySelector("#feni-flood-amount")
 const feniCount = document.querySelector(".feni-amount")
 
+// Quota
+const quotaFloodCount = document.querySelector("#quota-amount")
+const quataCount = document.querySelector(".quota-amount")
 
-function feniDonate(navCount) {
+
+// Feni donation card
+function feniDonate(navCount, cardTitle) {
     const feniInputNumber = parseFloat(feniCount.value)
     const feniNavCountNumber = parseFloat(navCount.innerText)
     const floodCountNumber = parseFloat(feniFloodCount.innerText) 
@@ -18,9 +23,68 @@ function feniDonate(navCount) {
         navCount.innerText = myFeniNewBlance
         feniCount.focus()
         feniCount.value = ''
+
+        // History
+        const historyItems = document.createElement("div")
+        historyItems.innerHTML=`
+            
+
+          <div class="p-5 border border-gray-300 rounded-md mb-7">
+                <p class="text-md font-bold mb-5"> ${feniFloodCount.innerText = feniNewBlance} <span class="text-3xl font-extrabold">৳</span> ${cardTitle.innerText}</p>
+
+                <p>${new Date()}</p>
+
+          </div>
+
+        `
+        historyContainer1.insertBefore(historyItems, historyContainer1.firstChild)
         
     }else{
         alert("Donate failed")
     }
 
+}
+// Quota donation card
+
+function quataDonate(navCount, cardTitle) {
+    const quataInputNumber = parseFloat(quataCount.value)
+    const quataNavCountNumber = parseFloat(navCount.innerText)
+    const qualaCountNumber = parseFloat(quotaFloodCount.innerText) 
+   
+    if(quataInputNumber > 0){
+        modal.classList.add("modal-open")          
+        const quataNewBlance =  qualaCountNumber + quataInputNumber 
+        quotaFloodCount.innerText = quataNewBlance
+        const myQuataNewBlance = quataNavCountNumber - quataInputNumber
+        
+        navCount.innerText = myQuataNewBlance
+        quataCount.focus()
+        quataCount.value = ''
+
+        // History
+        const historyItems = document.createElement("div")
+        historyItems.innerHTML=`
+            
+
+          <div class="p-5 border border-gray-300 rounded-md mb-7">
+                <p class="text-md font-bold mb-5"> ${feniFloodCount.innerText = feniNewBlance} <span class="text-3xl font-extrabold">৳</span> ${cardTitle.innerText}</p>
+
+                <p>${new Date()}</p>
+
+          </div>
+
+        `
+        historyContainer1.insertBefore(historyItems, historyContainer1.firstChild)
+        
+    }else{
+        alert("Donate failed")
+    }
+
+}
+
+// style donate & history button
+function historyButton(history, donate, historyContainer){
+    history.classList.add("active-btn-bg")
+    donate.classList.remove("active-btn-bg")
+    historyContainer.classList.remove("hidden")
 }
